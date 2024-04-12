3710e94a1794fa9c1042f30e113902f01248f69b
Jimena Talamantes <jimenatalamantes@Jimenas-MacBook-Pro-2.local>
2024-04-12 14:18:46 -0700

    added profile button and nav bar

--- bigfoot-admin/src/App.js
+++ bigfoot-admin/src/App.js
@@ -7,20 +7,22 @@ function App() {
   const [postHistory, setPostHistory] = useState([]);
 
   return (
-    <div> 
-        <nav>
-            <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/new-post">New Post</Link></li>
-              <li><Link to="/feedback">Feedback Form Responses</Link></li>
-            </ul>
-        </nav>
+    <div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/new-post">New Post</Link></li>
+          <li><Link to="/post-history">Post History</Link></li>
+          <li><Link to="/feedback">Feedback Form Responses</Link></li>
+          <li><Link to="/sos-announcment">Emergency Announcment</Link></li>
+        </ul>
+      </nav>
 
       <Routes>
-          <Route path="/new-post" element={<PostsAddPost postHistory={postHistory} setPostHistory={setPostHistory} />} />
-          <Route path="/" element={<HomePage postHistory={postHistory} />} />
+        <Route path="/new-post" element={<PostsAddPost postHistory={postHistory} setPostHistory={setPostHistory} />} />
+        <Route path="/" element={<HomePage postHistory={postHistory} />} />
       </Routes>
-    
+
     </div>
   );
 }
