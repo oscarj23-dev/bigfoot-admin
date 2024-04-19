494c170fc76216b5555c2a570baa9a28c22e7aa4
sophiappt <sophiapt@uw.edu>
2024-04-19 15:21:17 -0700

    initial firebase set up

---
+++ bigfoot-admin/src/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFHytmnV6hC2Jzanz5HxDbXSxecHKhB18",
+  authDomain: "capstone-1cb7a.firebaseapp.com",
+  projectId: "capstone-1cb7a",
+  storageBucket: "capstone-1cb7a.appspot.com",
+  messagingSenderId: "764045275528",
+  appId: "1:764045275528:web:4852b4d103aea6ab4cca3d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
